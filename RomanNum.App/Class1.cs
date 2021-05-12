79a764241d1c0c40db98d29866bd21c037410452
unknown <Lntinfotech@INFEDLBCP1387.lntinfotech.com>
2021-05-12 16:00:20 -0400

    roman num app

--- RomanNum.App/Class1.cs
+++ RomanNum.App/Class1.cs
@@ -1,8 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RomanNum.App
 {
-    public class Class1
+    public static class RomanConverter
     {
+        static Dictionary<string, int> romanMap = new Dictionary<string, int>()
+        {
+            {"I", 1},
+            {"V", 5},
+            {"X", 10},
+            {"L", 50},
+            {"C", 100},
+            {"D", 500},
+            {"M", 1000},
+        };
+
+        public static int Convert(string number) {
+            if (number == null) {
+                throw new ArgumentNullException();
+            }
+
+            foreach (char c in number) {
+                if (!romanMap.ContainsKey(number)) {
+                    throw new ApplicationException("Invalid Input");
+                }
+            }
+
+            return romanMap[number];
+        }
     }
 }
