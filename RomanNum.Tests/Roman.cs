a2630b41b26415892e9187e06a429a0fa9a55eaa
unknown <Lntinfotech@INFEDLBCP1387.lntinfotech.com>
2021-05-12 15:58:17 -0400

    implemented Convert function

--- RomanNum.Tests/Roman.cs
+++ RomanNum.Tests/Roman.cs
@@ -1,16 +1,45 @@
 using System;
 using Xunit;
+using RomanNum.App;
+using FluentAssertions;
+using System.Collections.Generic;
 
 namespace RomanNum.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ConversionFunctionReturns()
         {
-            string roman = "V";
+            string roman = null;
 
+            Action action = () => RomanConverter.Convert(roman);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void ConvertsSingleRomanNumeral()
+        {
+            //Given
+            string roman = "V";
+            //When
             var conversion = RomanConverter.Convert(roman);
+            //Then
+            conversion.Should().Be(5);
+        }
+
+        [Fact]
+        public void HandlesInvalidInput()
+        {
+        //Given
+            string roman = "PICKLE RICK";
+            Action action = () => RomanConverter.Convert(roman);
+        //When
+
+        //Then
+            action.Should().Throw<ApplicationException>()
+            .WithMessage("Invalid Input");
         }
     }
 }
